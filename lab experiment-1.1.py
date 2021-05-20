7d30c0c8cdf29d558226be61daa9511da8f02ec8
garapati vamshidhar <84447482+garapati-2002@users.noreply.github.com>
2021-05-20 12:02:09 +0530

    Add files via upload

---
+++ lab experiment-1.1.py
@@ -0,0 +1,22 @@
+a=int(input())
+b=int(input())
+c=int(input())
+total=a+b+c
+average=total/3
+print("total is",total)
+print("average is",average)
+
+
+#expected output
+#9
+#3
+#6
+#total is 18
+#average is 6.0
+
+#original output
+#9
+#3
+#6
+#total is 18
+#average is 6.0
