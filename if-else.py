06a4cde5a95e29b9ecaaba46a786c27b951d712b
garapati vamshidhar <84447482+garapati-2002@users.noreply.github.com>
2021-05-22 13:46:27 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,13 @@
+#IF logic
+a=2
+if a<7:
+  print('a is less than 7')
+  print('hello....')
+  
+'''Expected output: 
+a is less than 7
+hello....'''
+
+'''Actual output:
+a is less than 7
+hello....'''
