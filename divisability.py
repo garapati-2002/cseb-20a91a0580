866468eb313e9ea19dd67310a5616eb65f64d9c3
garapati vamshidhar <84447482+garapati-2002@users.noreply.github.com>
2021-05-22 14:31:41 +0530

    Add files via upload

---
+++ divisability.py
@@ -0,0 +1,16 @@
+no=int(input('enter number'))
+if no%2==0:
+      print('number is divisible by 2')
+elif no%3==0:
+     print('number is divisible by 3')
+elif no%4==0:
+     print('number is divisible by 4')
+elif no%5==0:
+     print('number is divisible by 5')
+else:
+      print('Number is not divisible by 2,3,4 and 5')
+
+
+output:
+enter number 22
+number is divisible by 2
