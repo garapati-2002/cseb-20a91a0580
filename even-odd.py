457d26ba544b4670b20efac4120c45655583fbf4
garapati vamshidhar <84447482+garapati-2002@users.noreply.github.com>
2021-05-22 13:54:48 +0530

    Add files via upload

---
+++ even-odd.py
@@ -0,0 +1,11 @@
+a=int(input('enter number '))
+if a%2==0 :
+    print('a is an even number')
+else :
+    print('a is an odd number')
+
+#expected output
+#enter number 342
+#a is an even number
+#enter number 345
+#a is an odd number  
